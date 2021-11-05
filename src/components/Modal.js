c539b208c92e668475f4d68d5aab9f03a625a54d
darkdevil18 <sampaddibya@gmail.com>
2021-11-05 10:36:38 +0530

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 const MyModal = (props) => {
   return (
     <div
       style={{
-        position: "absolute",
-        width: "100vw",
+        position: "fixed",
+        width: "100%",
         height: "100vh",
-        backdropFilter: "blur(5px)",
+        backdropFilter: "blur(10px)",
         WebkitBackdropFilter: "blur(10px)",
-        padding: "4px 10px",
         display: "flex",
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        zIndex: '2000',
       }}
     >
       <main
@@ -21,13 +21,20 @@ const MyModal = (props) => {
           background: "white",
           Width: "400px",
           margin: "auto",
-          zIndex: "100",
           borderRadius: "1rem",
-          padding: "10px 16px"
+          padding: "32px 24px"
         }}
       >
-        <h1>Attention Viewer!!!</h1>
-        <h3>
+        <h1
+          style={{
+            margin: '1.5rem 0'
+          }}
+        >Attention Viewer!!!</h1>
+        <h3
+          style={{
+            margin: '1.5rem 0'
+          }}
+        >
           This is a clone of <a href="https://www.tesla.com/">tesla.com</a> and
           not the original website.
         </h3>
